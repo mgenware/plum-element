7464004ae27852960c9f87ecd8ca6b165f264e93
Mgen <m@mgenware.com>
2021-01-17 00:31:35 +0800

    Add callbacks for property and attribute updates

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
+/* eslint-disable class-methods-use-this */
 export type PlumPropType = 's' | 'n' | 'b' | 'a' | 'o';
-export type PlumProps = Record<string, PlumPropType>;
+export type PlumPropDefs = Record<string, PlumPropType>;
 
+/**
+ * Main type of plum-element.
+ *
+ * Some terms used in this file:
+ * `pl-prop`: a property defined in a `PlumElement`.
+ */
 export class PlumElement extends HTMLElement {
   // Returns a map of pl-props to monitor for changes.
-  static get plProps(): PlumProps {
+  static get plProps(): PlumPropDefs {
     return {};
   }
 
@@ -12,44 +19,122 @@ export class PlumElement extends HTMLElement {
   }
 
   // Stores all pl-props in a map.
-  protected plProps: Record<string, unknown> = {};
+  protected plPropValues: Record<string, unknown> = {};
 
   // Gets the value of a specified pl-prop.
   protected getPLProp(prop: string): unknown {
-    return this.plProps[prop];
+    return this.plPropValues[prop];
+  }
+
+  // Only called by `setPLProp` to set or remove the specified attribute.
+  private setPLAttributeInternal(name: string, value: string | null) {
+    if (value === null) {
+      this.removeAttribute(name);
+    } else {
+      this.setAttribute(name, value);
+    }
+  }
+
+  protected mustGetPLPropType(name: string): PlumPropType {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const propType = (this.constructor as any).plProps[name] as PlumPropType | undefined;
+    if (!propType) {
+      throw new Error(`Prop "${name}" is not defined`);
+    }
+    return propType;
   }
 
   // Sets the value of a specified pl-prop.
-  // `ignoreAttr`: true if attribute update is not needed.
-  protected setPLProp(prop: string, value: unknown, ignoreAttr = false) {
-    const current = this.getPLProp(prop);
-    // Treat `null` as `undefined` (default property value).
-    if (value === null) {
-      // eslint-disable-next-line no-param-reassign
-      value = undefined;
-    }
-    if (current !== value) {
-      this.plProps[prop] = value;
-      if (!ignoreAttr) {
-        if (value === undefined) {
-          this.removeAttribute(prop);
-          return;
-        }
-        if (typeof value === 'boolean') {
-          if (value) {
-            this.setAttribute(prop, '');
-          } else {
-            this.removeAttribute(prop);
-          }
-        } else {
-          this.setAttribute(prop, `${value}`);
-        }
+  protected setPLProp(
+    name: string,
+    value: unknown,
+    // Determines if this pl-prop change is triggered by an attribute change.
+    attrInfo?: {
+      oldValue: string | null;
+      newValue: string | null;
+    },
+  ) {
+    const currentPropValue = this.getPLProp(name);
+    if (currentPropValue !== value) {
+      this.plPropValues[name] = value;
+      this.onPLPropUpdated(name, currentPropValue, value, !!attrInfo);
+
+      // `onPLPropUpdated` and `onPLAttributeUpdated` must fire in pairs.
+      if (!attrInfo) {
+        const attrValue = this.plPropToAttribute(value, this.mustGetPLPropType(name));
+        this.setPLAttributeInternal(name, attrValue);
+        this.onPLAttributeUpdated(name, null, attrValue, false);
+      } else {
+        // Here, prop change is triggered by an attribute change, no need to update
+        // attribute again. But we still need to call `onPLAttributeUpdated`.
+        this.onPLAttributeUpdated(name, attrInfo.oldValue, attrInfo.newValue, true);
       }
     }
   }
 
-  attributeChangedCallback(name: string, _oldValue: string | null, newValue: string | null) {
+  attributeChangedCallback(name: string, oldValue: string | null, newValue: string | null) {
+    // Get prop value from attribute value.
+    const propValue = this.plAttributeToProp(newValue, this.mustGetPLPropType(name));
     // Sync attributes with props.
-    this.setPLProp(name, newValue, false);
+    this.setPLProp(name, propValue, { oldValue, newValue });
+  }
+
+  // Called when a pl-prop is updated.
+  // `attrToPropUpdate` true if this pl-prop change is triggered by an attribute change.
+  protected onPLPropUpdated(
+    _name: string,
+    _oldValue: unknown,
+    _newValue: unknown,
+    _attrToPropUpdate: boolean,
+  ) {}
+
+  // Called when an attribute is updated in response to a pl-prop update.
+  // `attrToPropUpdate` true if this pl-prop change is triggered by an attribute change.
+  protected onPLAttributeUpdated(
+    _name: string,
+    _oldValue: string | null,
+    _newValue: string | null,
+    _attrToPropUpdate: boolean,
+  ) {}
+
+  protected plAttributeToProp(value: string | null, type: PlumPropType): unknown {
+    switch (type) {
+      case 's':
+        return value ?? undefined;
+
+      case 'n':
+        return value === null ? undefined : parseInt(value, 10);
+
+      case 'b':
+        return value !== null;
+
+      case 'a':
+      case 'o':
+        return value === null ? undefined : JSON.parse(value);
+
+      default:
+        return value;
+    }
+  }
+
+  protected plPropToAttribute(value: unknown, type: PlumPropType): string | null {
+    if (value === null || value === undefined) {
+      return null;
+    }
+    switch (type) {
+      case 's':
+      case 'n':
+        return `${value}`;
+
+      case 'b':
+        return value === true ? '' : null;
+
+      case 'a':
+      case 'o':
+        return JSON.stringify(value);
+
+      default:
+        return `${value}`;
+    }
   }
 }
--- tests/main.test.ts
+++ tests/main.test.ts
@@ -1,11 +1,11 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 /* eslint-disable import/no-duplicates */
 import { html, fixture, expect } from '@open-wc/testing';
-import { PlumElement, PlumProps } from '..';
+import { PlumElement, PlumPropDefs } from '..';
 
 it('Basic syncing between props n attrs', async () => {
   class TElement extends PlumElement {
-    static get plProps(): PlumProps {
+    static get plProps(): PlumPropDefs {
       return {
         a: 's',
         b: 'n',
@@ -28,16 +28,110 @@ it('Basic syncing between props n attrs', async () => {
       this.setPLProp('b', v);
     }
   }
-  customElements.define('t-element', TElement);
-  const el: TElement = await fixture(html`<t-element></t-element>`);
+  customElements.define('t-basic', TElement);
+  const el: TElement = await fixture(html`<t-basic></t-basic>`);
 
   expect(el.getAttribute('a')).to.eq(null);
   expect(el.getAttribute('b')).to.eq(null);
+
+  // Prop -> attr.
+  el.a = 'mgenware';
+  el.b = 17;
   el.a = 'mgenware';
   el.b = 17;
   expect(el.getAttribute('a')).to.eq('mgenware');
   expect(el.getAttribute('b')).to.eq('17');
 
+  // Attr -> prop.
   el.setAttribute('a', 'hi');
+  el.setAttribute('b', '17');
   expect(el.a).to.eq('hi');
+});
+
+it('onPLPropUpdated and onPLAttrUpdated', async () => {
+  class TElement extends PlumElement {
+    propCount = 0;
+    attrCount = 0;
+    pName = '';
+    pOld: unknown;
+    pNew: unknown;
+    attrToPropUpdate = false;
+    aName = '';
+    aOld: unknown;
+    aNew: unknown;
+
+    static get plProps(): PlumPropDefs {
+      return {
+        a: 's',
+      };
+    }
+
+    get a(): unknown {
+      return this.getPLProp('a');
+    }
+
+    set a(v: unknown) {
+      this.setPLProp('a', v);
+    }
+
+    protected onPLPropUpdated(
+      name: string,
+      oldValue: unknown,
+      newValue: unknown,
+      attrToPropUpdate: boolean,
+    ) {
+      this.propCount++;
+      this.pName = name;
+      this.pOld = oldValue;
+      this.pNew = newValue;
+      this.attrToPropUpdate = attrToPropUpdate;
+    }
+
+    protected onPLAttributeUpdated(
+      name: string,
+      oldValue: string | null,
+      newValue: string | null,
+      attrToPropUpdate: boolean,
+    ) {
+      this.attrCount++;
+      this.aName = name;
+      this.aOld = oldValue;
+      this.aNew = newValue;
+      this.attrToPropUpdate = attrToPropUpdate;
+    }
+  }
+  customElements.define('t-pl-events', TElement);
+  const el: TElement = await fixture(html`<t-pl-events></t-pl-events>`);
+
+  // Prop -> attr.
+  el.a = 'mgenware';
+  expect(el.attrToPropUpdate).to.eq(false);
+  expect(el.pName).to.eq('a');
+  expect(el.pOld).to.eq(undefined);
+  expect(el.pNew).to.eq('mgenware');
+
+  expect(el.aName).to.eq('a');
+  expect(el.aOld).to.eq(null);
+  expect(el.aNew).to.eq('mgenware');
+
+  expect(el.propCount).to.eq(1);
+  expect(el.attrCount).to.eq(1);
+
+  expect(el.getAttribute('a')).to.eq('mgenware');
+
+  // Attr -> prop.
+  el.setAttribute('a', 'changed');
+  expect(el.attrToPropUpdate).to.eq(true);
+  expect(el.pName).to.eq('a');
+  expect(el.pOld).to.eq('mgenware');
+  expect(el.pNew).to.eq('changed');
+
+  expect(el.aName).to.eq('a');
+  expect(el.aOld).to.eq('mgenware');
+  expect(el.aNew).to.eq('changed');
+
+  expect(el.propCount).to.eq(2);
+  expect(el.attrCount).to.eq(2);
+
+  expect(el.a).to.eq('changed');
 });
